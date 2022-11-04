f138b9aaed7e9e14e3dcc56ebb8504c5db92bec1
Somajik <majida.jouida@gmail.com>
2022-11-04 17:23:11 +0100

    css

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,12 @@
 import '../css/app.scss';
+import { Dropdown } from 'bootstrap';
+
+document.addEventListener('DOMContentLoaded',() => { // JAVA* au moment ou le document est complétement chargé on apelle la fonction enableDropdowns activer les listes deroulantes();//
+    enableDropdowns();
+
+});
+
+const enableDropdowns = () => {
+    const dropdownElementList = document.querySelectorAll('.dropdown-toggle')
+ dropdownList = [...dropdownElementList].map(dropdownToggleEl => new Dropdown(dropdownToggleEl))
+};
