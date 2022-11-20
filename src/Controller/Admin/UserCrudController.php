dea1931cf233e0021dc4664ba6ba9dbeee589b6a
Somajik <majida.jouida@gmail.com>
2022-11-20 14:02:40 +0100

    f4

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -4,8 +4,12 @@ namespace App\Controller\Admin;
 
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Orm\EntityRepository;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -15,13 +19,28 @@ class UserCrudController extends AbstractCrudController
     }
 
     
+
+    
+
+
     public function configureFields(string $pageName): iterable
     {
-        return [
+        
            
-            TextField::new('username',),
-            TextEditorField::new('roles'),
-        ];
+        yield TextField::new('username',);
+        // yield TextField::new('password')->onlyOnForms()
+        // ->setFormType(PasswordType::class);// yield propre a easy admin//
+        yield ChoiceField::new('roles')
+        ->allowMultipleChoices()
+        ->renderAsBadges([
+            'ROLE_ADMIN' => 'success',
+            'ROLE_AUTHOR' => 'warning'
+        ])
+        ->setChoices([
+            'Administrateur' => 'ROLE_ADMIN',
+            'Auteur' => 'ROLE_AUTHOR'
+        ]);
+    
     }
     
 }
